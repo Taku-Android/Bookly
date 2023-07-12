04e5b0fd6af5e0e404df1f967079989b61b6dc5c
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-07-12 04:50:38 +0300

    first we use the get package to navigate throw the app
    second make the lib folder be two folder with clean arch
      - feature
      - core
    
    inside the feature
     - presentation  --> for the widgets this is how the mvvm do it
     - data  --> for any data local or remote
    
    inside the presentation
     - viewModels  --> for the viewModels in the app
     - views  --> and it have all the views and widgets

---
+++ lib/feature/persentation/views/splash_view.dart
@@ -0,0 +1,17 @@
+import 'package:bookly/feature/persentation/views/widgets/splash_widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
+
+
+
+
