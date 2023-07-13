5ad8f5a94d3fa67d3c4364f349329b0fd673f0b7
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-07-13 03:45:44 +0300

    removing set state as it rebuild all the view and this is not good for performance so we use the animationBuilder

--- lib/feature/presentation/views/widgets/splash_widgets/splash_view_body.dart
+++ lib/feature/presentation/views/widgets/splash_widgets/splash_view_body.dart
@@ -8,49 +8,48 @@ class SplashViewBody extends StatefulWidget {
   State<SplashViewBody> createState() => _SplashViewBodyState();
 }
 
-class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin{
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
 
-  late AnimationController animationController ;
   //to control the animation value as the animationController is get value from 0 - 1
-  late Animation<Offset> slidingAnimation ;
+  late Animation<Offset> slidingAnimation;
 
   @override
   void initState() {
     super.initState();
 
-    animationController = AnimationController(
-      vsync: this ,
-      duration: const Duration(seconds: 2)
-    );
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
 
-    slidingAnimation = Tween<Offset>(
-        begin: const Offset( 0 , 2) ,
-        end: Offset.zero 
-    ).animate(animationController);
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
 
     animationController.forward();
-    slidingAnimation.addListener(() {
-      setState(() {
-
-      });
-    });
 
   }
 
   @override
   Widget build(BuildContext context) {
-    return SlideTransition(
-      position: slidingAnimation,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Image.asset(AssetsPaths.logo),
-          const Text('Read Free Books'  ,
-            textAlign: TextAlign.center,)
-
-        ],
-      ),
+    return AnimatedBuilder(
+      animation: slidingAnimation,
+      builder: (context, _) {
+        return SlideTransition(
+          position: slidingAnimation,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.asset(AssetsPaths.logo),
+              const Text(
+                'Read Free Books',
+                textAlign: TextAlign.center,
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
