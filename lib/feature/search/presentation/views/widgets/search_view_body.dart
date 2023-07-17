5ec0f61abca7d991813efacaf11c562a9934cb61
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-07-17 07:29:02 +0300

    adding  close icon

--- lib/feature/search/presentation/views/widgets/search_view_body.dart
+++ lib/feature/search/presentation/views/widgets/search_view_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly/core/utils/Styles.dart';
 import 'package:bookly/feature/search/presentation/views/widgets/custom_search_text_field.dart';
 import 'package:bookly/feature/search/presentation/views/widgets/search_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
@@ -14,12 +15,26 @@ class SearchViewBody extends StatelessWidget {
           slivers: [
             SliverToBoxAdapter(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children:  [
+
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0 , top: 10 , bottom: 15),
+                    child: GestureDetector(
+                        onTap: (){
+                          GoRouter.of(context).pop();
+                        },
+                        child: const Icon(Icons.close , size: 32,)),
+                  ) ,
+
                    const CustomSearchTextField(),
 
-                   Text('Results', style: Styles.textStyle30.copyWith(
-                     fontWeight: FontWeight.w700
-                   ),),
+                   Padding(
+                     padding: const EdgeInsets.only(left: 30.0 , bottom: 15),
+                     child: Text('Results', style: Styles.textStyle30.copyWith(
+                       fontWeight: FontWeight.w700
+                     ),),
+                   ),
                 ],
               ),
             ),
