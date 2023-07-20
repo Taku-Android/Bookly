e54744125e67afd4d64c4095da18a8bff606f95a
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-07-21 02:56:47 +0300

    provide bloc to the main

--- lib/feature/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
+++ lib/feature/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -1,4 +1,3 @@
-
 import 'package:bookly/feature/home/data/repo/home_repo.dart';
 import 'package:bookly/feature/home/presentation/manager/newest_books_cubit/newest_books_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,18 +5,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class NewestBooksCubit extends Cubit<NewestBooksState> {
   NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
 
-  final HomeRepo homeRepo ;
+  final HomeRepo homeRepo;
 
   Future<void> fetchNewestBooks() async {
-
     emit(NewestBooksLoading());
     var result = await homeRepo.fetchNewSetBook();
 
-    result.fold(
-            (failure) => emit(NewestBooksFailure(failure.errMessage)),
-            (books) => emit(NewestBooksSuccess(books)));
-
-
+    result.fold((failure) => emit(NewestBooksFailure(failure.errMessage)),
+        (books) => emit(NewestBooksSuccess(books)));
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:bookly/constants.dart';
+import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/core/utils/app_routes.dart';
+import 'package:bookly/feature/home/data/repo/home_repo_impl.dart';
+import 'package:bookly/feature/home/presentation/manager/feature_books_cubit/feature_books_cubit.dart';
+import 'package:bookly/feature/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -12,16 +18,35 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: AppRoutes.router,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeatureBooksCubit(
+            HomeRepoImpl(
+              ApiService(
+                Dio(),
+              ),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(
+            HomeRepoImpl(
+              ApiService(
+                Dio(),
+              ),
+            ),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: AppRoutes.router,
+        theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: kPrimaryColor,
+            textTheme:
+                GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
       ),
-
     );
   }
 }
-
-
