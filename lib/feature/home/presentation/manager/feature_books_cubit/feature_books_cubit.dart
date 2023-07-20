38d4d94efccf228eecb13f0e9ccea5180be91bc0
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-07-21 01:22:00 +0300

    fetchFeatureBooks using the homeRepo
    adding my stats

--- lib/feature/home/presentation/manager/feature_books_cubit/feature_books_cubit.dart
+++ lib/feature/home/presentation/manager/feature_books_cubit/feature_books_cubit.dart
@@ -1,9 +1,23 @@
 
+import 'package:bookly/feature/home/data/repo/home_repo.dart';
 import 'package:bookly/feature/home/presentation/manager/feature_books_cubit/feature_books_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeatureBooksCubit extends Cubit<FeatureBooksState> {
-  FeatureBooksCubit() : super(FeatureBooksInitial());
+  FeatureBooksCubit(this.homeRepo) : super(FeatureBooksInitial());
 
+  final HomeRepo homeRepo ;
+
+  Future<void> fetchFeatureBooks() async {
+
+    emit(FeatureBooksLoading());
+    var result = await homeRepo.fetchFeatureBooks();
+
+    result.fold(
+            (failure) => emit(FeatureBooksFailure(failure.errMessage)),
+            (books) => emit(FeatureBooksSuccess(books)));
+
+
+  }
 
 }
--- lib/feature/home/presentation/manager/feature_books_cubit/feature_books_state.dart
+++ lib/feature/home/presentation/manager/feature_books_cubit/feature_books_state.dart
@@ -1,15 +1,23 @@
 
+import '../../../data/model/BookModel.dart';
+
 abstract class FeatureBooksState {}
 
 class FeatureBooksInitial extends FeatureBooksState {}
 
 
 class FeatureBooksLoading extends FeatureBooksState {}
-class FeatureBooksSuccess extends FeatureBooksState {}
+class FeatureBooksSuccess extends FeatureBooksState {
+
+  final List<BookModel> books ;
+
+  FeatureBooksSuccess(this.books);
+
+}
 class FeatureBooksFailure extends FeatureBooksState {
 
-  String errMessage ;
+  final String errMessage ;
 
-  FeatureBooksFailure(this.errMessage);
+   FeatureBooksFailure(this.errMessage);
 
 }
