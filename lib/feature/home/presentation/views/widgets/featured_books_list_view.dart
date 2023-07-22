2ebdf12f6a8b9398ed72edba18fe3e20a730b9f7
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-07-22 14:26:25 +0300

    making stats of the bloc to get the featured_books_list_view.dart

--- lib/feature/home/presentation/views/widgets/featured_books_list_view.dart
+++ lib/feature/home/presentation/views/widgets/featured_books_list_view.dart
@@ -1,21 +1,39 @@
+import 'package:bookly/core/utils/widgets/custom_error_widget.dart';
+import 'package:bookly/feature/home/presentation/manager/feature_books_cubit/feature_books_cubit.dart';
+import 'package:bookly/feature/home/presentation/manager/feature_books_cubit/feature_books_state.dart';
 import 'package:bookly/feature/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
   const FeaturedBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.width * .55,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.only(right: 16.0),
-              child: CustomBookImage(),
-            );
-          }),
+    return BlocBuilder<FeatureBooksCubit, FeatureBooksState>(
+      builder: (context, state) {
+        if(state is FeatureBooksSuccess){
+          return SizedBox(
+            height: MediaQuery
+                .of(context)
+                .size
+                .width * .55,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.only(right: 16.0),
+                    child: CustomBookImage(),
+                  );
+                }),
+          );
+        }else if(state is FeatureBooksFailure) {
+          return Center(child: CustomErrorWidget(errMessage: state.errMessage));
+        }else{
+          return const Center(child: CircularProgressIndicator());
+        }
+
+      },
     );
   }
 }
