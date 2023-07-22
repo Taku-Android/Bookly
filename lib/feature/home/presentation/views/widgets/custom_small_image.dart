de9768bb4d13159510a926af286d42fd6f8e454f
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-07-22 17:44:24 +0300

    use  the cached image into the border

--- lib/feature/home/presentation/views/widgets/custom_small_image.dart
+++ lib/feature/home/presentation/views/widgets/custom_small_image.dart
@@ -1,24 +1,23 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-
-import '../../../../../core/utils/assets.dart';
 
 class CustomSmallImage extends StatelessWidget {
   const CustomSmallImage({
-    super.key,
+    super.key, required this.imageUrl,
   });
+
+  final String imageUrl ;
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2 / 3,
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(16),
-            image: const DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage(AssetsPaths.book1),
-            )),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: AspectRatio(
+        aspectRatio: 2 / 3,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+        ),
       ),
     );
   }
